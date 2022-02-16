610e38f086c66d31809811ff61185081d8ae73a0
MFlorO <floroldani@hotmail.com>
2022-02-16 15:11:16 -0300

    nuevos cambios homework JS1 y JS2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -237,9 +237,8 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length !== 1){
-    return "Dato Incorrecto";
-  } else if (letra === "a" ||letra === "e" || letra === "i" ||letra === "o" ||letra === "u"){
+  if (letra.length !== 1) {return "Dato Incorrecto"};
+  if (letra === "a" ||letra === "e" || letra === "i" ||letra === "o" ||letra === "u"){
     return "Es vocal";
   } else {
     return "Dato incorrecto";
