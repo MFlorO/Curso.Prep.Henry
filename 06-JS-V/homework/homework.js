e9dc846b409f44f5c8e5a531adf1094bff0f1b06
MFlorO <floroldani@hotmail.com>
2022-02-23 22:10:41 -0300

    Homework JSV 3

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,15 +8,15 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
-  function Usuario (usuario, nombre, email, password){
-    this.usuario = usuario;
-    this.nombre = nombre;
-    this.email = email;
-    this.password = password
+  function Usuario (opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password
     }
 
   Usuario.prototype.saludar =  function () {
-    return "Hola, mi nombre es" + this.nombre ;
+    return "Hola, mi nombre es " + this.nombre ;
   }
 
   return Usuario;
@@ -38,7 +38,7 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
   String.prototype.reverse = function(){
-    var palabraInvertida = " ";
+    var palabraInvertida = "";
     for (var i = this.length-1; i >= 0; i--){
       palabraInvertida = palabraInvertida + this[i]
     }
